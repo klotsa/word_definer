5b7156828ebef54d43815a8a8d17725fe1d9270f
Anatoliy Klots <klots@uw.edu>
2017-04-28 10:16:07 -0700

    add a project structure and pass a second test returning all words of the array

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -8,10 +8,18 @@ describe('words') do
 
   describe(".clear") do
     it "returns an empty array" do
-      test_word = Words.new({:word=>"word"})
+      test_word = Words.new({:word=>"ocean"})
       test_word.save()
       Words.clear()
       expect(Words.all()).to(eq([]))
+    end
+  end
+
+  describe(".all") do
+    it "returns all words" do
+      test_word = Words.new({:word=>"ocean"})
+      test_word.save()
+      expect(Words.all()).to(eq([test_word]))
     end
   end
 
