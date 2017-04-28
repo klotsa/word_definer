1048cb359143bb25935822d33c33ec0746656bc0
Anatoliy Klots <klots@uw.edu>
2017-04-28 16:21:17 -0700

    add minimal styling

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -1,49 +1,49 @@
 require('rspec')
-require('words')
+require('word')
 
-describe('words') do
+describe('Word') do
   before() do
-    Words.clear()
+    Word.clear()
   end
 
   describe(".clear") do
     it "returns an empty array" do
-      test_word = Words.new({:word=>"ocean"})
+      test_word = Word.new({:word=>"ocean"})
       test_word.save()
-      Words.clear()
-      expect(Words.all()).to(eq([]))
+      Word.clear()
+      expect(Word.all()).to(eq([]))
     end
   end
 
   describe(".all") do
     it "returns all words" do
-      test_word = Words.new({:word=>"ocean"})
+      test_word = Word.new({:word=>"ocean"})
       test_word.save()
-      expect(Words.all()).to(eq([test_word]))
+      expect(Word.all()).to(eq([test_word]))
     end
   end
 
   describe("#save") do
     it "saves a word to the class variable" do
-      test_word = Words.new({:word=>"ocean"})
+      test_word = Word.new({:word=>"ocean"})
       test_word.save()
-      expect(Words.all()).to(eq([test_word]))
+      expect(Word.all()).to(eq([test_word]))
     end
   end
 
   describe(".find") do
    it("returns a word by its id number") do
-     test_word = Words.new({:word=>"ocean"})
+     test_word = Word.new({:word=>"ocean"})
      test_word.save()
-     test_word2 = Words.new({:word=>"mountain"})
+     test_word2 = Word.new({:word=>"mountain"})
      test_word2.save()
-     expect(Words.find(test_word.id())).to(eq(test_word))
+     expect(Word.find(test_word.id())).to(eq(test_word))
    end
  end
 
  describe("#id") do
    it "returns the word's id" do
-     test_word = Words.new({:word=>"ocean"})
+     test_word = Word.new({:word=>"ocean"})
      expect(test_word.id()).to(eq(1))
    end
  end
