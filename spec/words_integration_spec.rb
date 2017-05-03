03d2e9cccb29b2745a256d272fd6b577415700a9
klotsa <klots@uw.edu>
2017-05-02 18:36:07 -0700

    Update words_integration_spec.rb

--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -4,31 +4,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-
-
-describe('word', {:type => :feature}) do
-  it('processes the user entry and add a word to the list of words') do
-    visit('/')
-    click_on('Add a New Word')
-    expect(page).to have_content('Word to Learn:')
-  end
-
-  it('processes user entry and add it to a list of words') do
+describe('name', {:type => :feature}) do
+  it('processes user entry, adds it to a list of words, and shows a confirmation msg') do
     visit('/words/new')
-    fill_in('word', :with => "ocean")
+    fill_in("name", :with => "ocean")
     click_button('Add it!')
-    expect(page).to have_content('ocean')
-  end
-
-  it('processes user entry and promts for a word definition') do
-    visit('/words')
-    click_on('ocean')
-    expect(page).to have_content('Add a new definition')
-  end
-
-  it('processes user entry shows the definition(s) of the word') do
-  visit('/words/1')
-  click_button('ocean')
-  expect(page).to have_content('lots of salty water')
+    expect(page).to have_content('Hooray!')
   end
 end
