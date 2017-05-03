7711f936208ea60a9f734d81b33b84f44b9961d0
klotsa <klots@uw.edu>
2017-05-02 18:53:43 -0700

    Update words_integration_spec.rb
    add anew word test and a confirmation tests

--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -4,6 +4,15 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+
+describe('the word definer page', {:type => :feature}) do
+  it('give an option to add a new word') do
+    visit('/')
+    click_link('Add a New Word to Learn')
+    expect(page).to have_content('Add a New Word')
+  end
+end
+
 describe('name', {:type => :feature}) do
   it('processes user entry, adds it to a list of words, and shows a confirmation msg') do
     visit('/words/new')
